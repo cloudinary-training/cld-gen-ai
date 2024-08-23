e48ec2d88bf499880ae85dc11e7d72be5f80f0de
Jen Brissman <brissman514@gmail.com>
2024-08-22 22:44:58 -0700

    updating code

--- genUpscale.js
+++ genUpscale.js
@@ -6,6 +6,7 @@ cloudinary.config({ secure: "true" });
 
 // Note: To use the upscale effect, the input image must be
 // smaller than 4.2 megapixels (the equivalent of 2048 x 2048 pixels).
+// There is no direct way to correlate pixel size and file size.
 
 // Reference: https://cloudinary.com/documentation/effects_and_artistic_enhancements#upscale
 
